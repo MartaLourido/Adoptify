8f09eaf15560312fd4ca7c888cfe981778605bde
MartaLourido <martalouri@gmail.com>
2020-08-10 11:28:50 +0200

    dogcreate form done

--- routes/shelter.routes.js
+++ routes/shelter.routes.js
@@ -6,3 +6,15 @@ const bcryptjs = require('bcryptjs')
 const shelterModel = require("../models/shelter.model");
 //const for require adopter model
 const adopterModel = require("../models/adopter.model");
+
+//const for require dog model
+const dogModel = require("../models/dog.model");
+
+router.get('/pet-profile/create', (req, res) => {
+  res.render('dogcreate.hbs');
+})
+
+router.post('/pet-profile/create', (req, res) => {
+  dogModel.create
+  
+})
