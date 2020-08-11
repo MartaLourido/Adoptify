e761a8323fac235d311ba92dba1388917f551fad
MartaLourido <martalouri@gmail.com>
2020-08-11 10:04:45 +0200

    fixed somethings

--- routes/adopter.routes.js
+++ routes/adopter.routes.js
@@ -7,4 +7,84 @@ const shelterModel = require("../models/shelter.model");
 //const for require adopter model
 const adopterModel = require("../models/adopter.model");
 
-module.exports = model('User', userSchema);
+//const for require dog model
+const dogModel = require("../models/dog.model");
+
+
+//adopter tiene que ir al finder, tambien el shelter va al finder
+//
+
+let CITIES = ['Álava', 'Albacete','Alicante','Almería', 'Asturias','Ávila','Badajoz','Barcelona','Burgos','Cáceres','Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','A Coruña','Cuenca','Gerona','Granada','Guadalajara','Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra','Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza'];
+let SIZE = ['small', 'medium', 'large']
+//edit an delete adopter --> route /editadopter
+
+router.get('/adopter', (req, res) => {
+  res.render('adopter.hbs', {adopter: req.session.loggedInUser})
+})
+
+router.get('/adopter/editadopter', (req, res) => {
+  res.render('editadopter.hbs', {adopter: req.session.loggedInUser})
+})
+
+//edit profile
+
+router.post("/adopter", (req, res) => {
+  let adopterData = req.session.loggedInUser
+  const {name, location} = req.body
+
+  dogModel.findByIdAndUpdate( adopterData._id,{name, location})
+    .then((result) => {
+      dogModel.findById(adopterData._id)
+        .then((theResult) => {
+          theResult = req.session.loggedInUser 
+          res.redirect("/adopter")
+        })
+      
+      })
+
+})
+
+// Delete profile
+
+router.get('/adopter/deleteadopter/:id', (req, res) => {
+  const {id} = req.params;
+  adopterModel.findByIdAndDelete(id)
+    .then(() => res.redirect('/adopter'))
+    .catch((err) => {
+      console.log(`Error while deleting the profile: ${err}`);
+      next();
+    });
+});
+
+// .deleteOne({ name: "adopter" })
+// .then(() => {(
+//   console.log("Your  profile has been deleted"));
+// })
+
+
+//obteniendo el pet profile por id
+router.get('/petprofile/:dogId', (req, res) => {
+  dogModel.findById(req.params.dogId) 
+  .then ((dog) => { 
+    res.render ('petprofile.hbs', {dog})
+  })
+
+})
+
+//doing the filter from the adopter, still need do it from the adopter as well
+router.get('/adopter/find-dogadopter', (req, res) => {
+  console.log('pasa por aqui');
+  res.render('find-dogadopter.hbs', {CITIES:CITIES,Size:SIZE})
+});
+
+router.post('/adopter/find-dogadopter', (req, res) => {
+  const {cities, size} = req.body;
+  dogModel.find({location: cities, size: SIZE})
+  .then ((result) =>
+    res.render('find-dogadopter.hbs', {CITIES:CITIES,size:SIZE, dogs: result}));
+})
+ 
+
+
+
+module.exports = router;
--- routes/shelter.routes.js
+++ routes/shelter.routes.js
@@ -10,14 +10,27 @@ const adopterModel = require("../models/adopter.model");
 //const for require dog model
 const dogModel = require("../models/dog.model");
 
+let CITIES = ['Álava', 'Albacete','Alicante','Almería', 'Asturias','Ávila','Badajoz','Barcelona','Burgos','Cáceres','Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','A Coruña','Cuenca','Gerona','Granada','Guadalajara','Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra','Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza'];
+
+let SIZE = ['small', 'medium', 'large']
+
+//create and edit petprofile
 router.get('/pet-profile/create', (req, res) => {
-  res.render('dogcreate.hbs');
+  res.render('dogcreate.hbs', {CITIES})
 })
 
-// router.get('/doglist', (req, res) => {
-//   res.render('doglist.hbs');
-// })
+router.get('/pet-profile/editdog', (req, res) => {
+  res.render('editdog.hbs', {CITIES})
+})
 
+router.get('/doglist', (req, res) => {
+  dogModel.find({shelter: req.session.loggedInUser._id})
+  .then((dogs) => {
+  res.render('doglist.hbs', {dogs})
+})
+})
+
+//obteniendo el pet profile por id
 router.get('/petprofile/:dogId', (req, res) => {
     dogModel.findById(req.params.dogId) 
     .then ((dog) => { 
@@ -26,15 +39,21 @@ router.get('/petprofile/:dogId', (req, res) => {
   
 })
 
-
-
+// //edit get y post
+// router.get('/petprofile/:id/editdog', (req, res, next) => {
+//   //Update the dog
+//   // ... your code here
+//   dog.findById(req.params.id)
+//     .then(dog => res.render('petprofile.hbs', {dog})
+//     .catch(e => console.error(e))
+// })
 
 router.post('/pet-profile/create', (req, res) => {
   const {
     shelter, name, age, size, description, cities, gender, goodwkids, goodwdogs, other
   } = req.body
   console.log(req.body)
-  dogModel.create ({shelter, name, age, size, description, cities, gender, goodwkids: goodwkids == 'on', goodwdogs: goodwdogs == 'on', other})
+  dogModel.create ({shelter: req.session.loggedInUser._id, name, age, size, description, cities, gender, goodwkids: goodwkids == 'on', goodwdogs: goodwdogs == 'on', other})
   .then ((dog) =>
     res.redirect(`/petprofile/${dog._id}`)
   )
@@ -42,5 +61,74 @@ router.post('/pet-profile/create', (req, res) => {
     console.log('Error ', err)
   })
 })
+
+
+//edit get y post
+router.get('/petprofile/:dogId/editdog', (req, res, next) => {
+  //Update the dog
+  // ... your code here
+  dogModel.findById(req.params.dogId)
+    .then(dog => res.render('editdog.hbs', {dog}))
+    .catch(e => console.error(e))
+})
+
+router.post('/petprofile/:dogId/editdog', (req, res, next) => {
+  // Update the dog profile
+  const {
+    shelter, name, age, size, description, cities, gender, goodwkids, goodwdogs, other
+  } = req.body
+  dogModel.findByIdAndUpdate(
+    {_dogId: req.params.dogId},
+    {
+      shelter, name, age, size, description, cities, gender, goodwkids, goodwdogs, other
+    }
+  )
+  .then(() => res.redirect(`/petprofile/${req.params.dogId}`) )
+  .catch(() => res.redirect(`/petprofile/${req.params.dogId}/edit`))
+});
+
+//delete un post sencillo
+
+router.get('/petprofile/:dogId/deletedog', (req, res, next) => {
+  // delete a dog
+  dogModel.findByIdAndDelete(
+    {_id: req.params.dogId}
+  )
+  .then(() => res.redirect('/shelter'))
+  .catch(() => res.redirect(`/petprofile/${req.params.dogId}/edit`))
+});
+
+
+//doing the filter from the shelter, still need do it from the adopter as well
+router.get('/shelter/find-dog', (req, res) => {
+  res.render('find-dog.hbs', {CITIES:CITIES,Size:SIZE})
+})
+
+router.post('/shelter/find-dog', (req, res) => {
+  const {cities, size} = req.body;
+  dogModel.find({size: size})
+  .then ((result) =>
+    res.render('find-dog.hbs', {CITIES:CITIES,size:SIZE, dogs: result})
+  )
+ 
+})
+
+//edit shelter profile
+
+
+//delete shelter profile
+
+router.post('/shelter/:id/deleteshelter', (req, res, next) => {
+  const { id } = req.params;
+  shelterModel.findByIdAndDelete(id)
+    .then(() => res.redirect('/shelter'))
+    .catch((err) => {
+      console.log(`Error while deleting a movie: ${err}`);
+      next();
+    });
+});
+
+
+
 
 module.exports = router;
